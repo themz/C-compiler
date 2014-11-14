2bc338406f8d8475793015b8bb8b44cf9113eccb
Michail Zinov <zinov.m.v@gmail.com>
2014-11-15 00:15:48 +1000

    Делаю счетчик пройденых тестов

--- tests/scannerTest.sh
+++ tests/scannerTest.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 cd scanner;
+all=0;
+ok=0;
 for file in $(ls | grep ".in$" | cut -d '.' -f 1); do
+	all=$[$all+1]
 	/Users/user/Desktop/University/c-compiler/Build/Products/Debug/c-compiler /s ${file}.in;
 	if diff ${file}.out ${file}.sol >/dev/null ; then
-  		echo ${file} OK;
+		ok=$[$ok+1]
+  		echo ${file} OK >/dev/null;
 	else
   		echo ${file} BAD
 	fi
 done
 cd ..
+echo '========== Scanner tests completed' $ok 'of' $all '==========';
