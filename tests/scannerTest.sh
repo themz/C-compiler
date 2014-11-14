c974cadb168c4c91580e6d8b513c944ebc82fd91
Michail Zinov <zinov.m.v@gmail.com>
2014-11-14 21:34:55 +1000

    Добавляю тесты для структур

--- tests/scannerTest.sh
+++ tests/scannerTest.sh
@@ -3,7 +3,7 @@ cd scanner;
 for file in $(ls | grep ".in$" | cut -d '.' -f 1); do
 	/Users/user/Desktop/University/c-compiler/Build/Products/Debug/c-compiler /s ${file}.in;
 	if diff ${file}.out ${file}.sol >/dev/null ; then
-  		echo ${file}
+  		echo ${file} OK;
 	else
   		echo ${file} BAD
 	fi
